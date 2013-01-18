23a55ded49b3494e500527352a78cc5a05c81db0
Tof37 <totofsen@gmail.com>
2013-01-18 23:03:26 +0100

    initial

---
+++ javascripts/ahah.js
@@ -0,0 +1,23 @@
+function ahah(url, target) {
+  var req;
+  if (window.XMLHttpRequest) {
+    req = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    req = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  if (req != undefined) {
+    req.onreadystatechange = function() {ahahDone(url, req, target);};
+    req.open("GET", url, true);
+    req.send("");
+  }
+}  
+
+function ahahDone(url, req, target) {
+  if (req.readyState == 4) { // only if req is "loaded"
+    if (req.status == 200) { // only if "OK"
+      document.getElementById(target).innerHTML = req.responseText;
+    } else {
+      document.getElementById(target).innerHTML="AHAH Error:\n"+ req.status + "\n" +req.statusText;
+    }
+  }
+}
